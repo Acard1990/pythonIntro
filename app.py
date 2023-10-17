c5267995e9374b8f71222dd7c7f2b01ed721ab7d
Scott Cardinali <scott.cardinali@gmail.com>
2023-10-17 17:17:53 -0400

    updated port

--- app.py
+++ app.py
@@ -51,7 +51,9 @@ def delete_user(id):
     db.session.commit()
     return redirect(url_for('index'))
 
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, port=port)
